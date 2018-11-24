d10a896cd280af32656b79c3471d3a283d91c493
Daniel Isaac <trebolman@gmail.com>
2018-11-23 19:00:53 -0500

    Semana 7 - dia 5

--- Semana7/Angular/colegio/src/app/c_alumno/alumno.component.ts
+++ Semana7/Angular/colegio/src/app/c_alumno/alumno.component.ts
@@ -7,21 +7,23 @@ import {Alumno} from '../modelos/Alumno'
 })
 export class AlumnoComponent{
     public alumnos:Array<Alumno>;
-    
+    public alumnoSeleccionado:Alumno;
     // Constructor
     constructor(){
         console.log("Alumno: Constructor ejecutado");
         this.alumnos = [
-            new Alumno(1,"Daniel","Cabana","0124",12),
-            new Alumno(2,"Isaac","Cabana","0124",12),
-            new Alumno(3,"Sebastian","Cabana","0124",12),
-            new Alumno(4,"Giannella","Cabana","0124",12),
-            new Alumno(5,"Giovanna","Cabana","0124",12),
-            new Alumno(6,"","Cabana","0124",12),
+            new Alumno(1,"Daniel","C","0124",12),
+            new Alumno(2,"Isaac","M","65",65),
+            new Alumno(3,"Sebastian","G","555",65),
+            new Alumno(4,"Foribundo","H","656",65),
+            new Alumno(5,"Giovanna","V","6565",65),
+            new Alumno(6,"sdsdsa","F","65",65),
         ];
         console.log(this.alumnos);
     }
 
     // Metodos
-
+    public onSelect(objAlumno:Alumno):void{
+        this.alumnoSeleccionado = objAlumno; //Cada vez que seleccione nuestra variable alumno sera igual al objeto alumno que acabo de seleccionar
+    }
 }
